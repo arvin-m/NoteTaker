49ddc73b7f37319020eb01ebea03432730f91c5f
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2020-02-19 23:22:52 -0500

    adding code for server/still working on it

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,1 +1,28 @@
 const fs = require("fs");
+// const $ = require("jquery");
+
+// add event listener to the save Btn
+$(".new-note").on("click", function() {
+ const newNote ={
+   id:0, //find a way to defined the specific id for each note
+   title:"",
+   note:""
+ };
+ 
+ const title=$(".note-title").val();
+ const note =$(".note-textarea").val();
+ 
+
+//  add new note 
+  $.post("/api/notes" , function(data) {
+    console.log(data);
+    if (data) {
+      $(".save-note").show();
+      title.text(newNote.title);
+      note.text(newNote.note);
+      
+    } else {
+    console.log("input cannot be empty !")
+    }
+  });
+});
--- server.js
+++ server.js
@@ -1,23 +1,53 @@
 const express=require("express");
 const path = require("path");
 const app =express();
-const PORT=process.env.PORT || 7030;
+const PORT=process.env.PORT || 7050;
 
 app.listen(PORT,()=>{
     console.log(` ----->>>>>>> Server Listening on port ${PORT} ! `)
 })
 
-const notesArr=[];
+const notesArr=[{
+    id:"1",
+    tittle:"arvin",
+    note:"heloo every one"
+
+}];
 
 // add the middleware I need to handle POST requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 // create a route for note.index file
 app.get("/notes",(req,res)=>{
-    res.sendFile(path.join(__dirname,"./public/notes.html") )
+    res.sendFile(path.join(__dirname,"./public/notes.html") );
 })
 // create a route for index.html file
 app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"./public/index.html") )
+    res.sendFile(path.join(__dirname,"./public/index.html") );
 })
+// create a route for load data from db.json
+app.get("/api/notes",(req,res)=>{
+    res.readFile(path.join(__dirname,"/db/db.json", "utf8") );
+})
+
+//create a route to add new note to the db.json
+app.post("/api/notes", (req, res) => {
+    //new note object is in req.body
+    const newNote = req.body;
+    // newNote.routeName = newNote.name.replace(/\s+/g, "").toLowerCase();
+    // console.log(newNote);
+    notesArr.push(newNote);
+    res.json(notesArr);
+         
+});
+
+// create a route to DELETE data
+app.delete("/api/notes/:id",(req,res)=>{
+    const noteBeDelete =req.body.id;
+    notesArr.pop(noteBeDelete); // find how to delet by specific id
+    res.send(notesArr);
+})
+
+
